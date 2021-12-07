b0dafe0409881372bfbf6712b3edbbe5aad66d96
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-12-07 15:55:38 +0100

    Day02
    Készíts a day02 csomagban egy StringLists osztályt és benne egy shortestWords(List<String> words) metódust, mely paraméterül kap egy String listát és a legrövidebb szavakat vissza adja belőle egy Listában. PL :{"aaa", "aa", "bb", "cccc", "dd"} Itt a legrövidebb szavak az "aa", "bb", "dd".

---
+++ src/main/java/Day02/StringList.java
@@ -0,0 +1,25 @@
+package Day02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringList {
+
+    public List<String> shortestWords(List<String> words) {
+
+        List<String> result = new ArrayList<>();
+
+        int minLength = Integer.MAX_VALUE;
+        for (String word : words) {
+            if (word.length() < minLength) {
+                minLength = word.length();
+                result.clear();
+            }
+            if (word.length() == minLength) {
+                result.add(word);
+            }
+        }
+        return result;
+    }
+}
+
